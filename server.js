c28533137d8a3221bc799d4f013756c9bd5cc4e7
AbbasAyda <abbasayda@gmail.com>
2022-03-27 20:41:31 +0530

    commit

--- server.js
+++ server.js
@@ -7,33 +7,36 @@ const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 
 
-const PORT = process.env.PORT || 3100
-//const MongoClient = require('mongodb').MongoClient;
+
+const MongoClient = require('mongodb').MongoClient;
 
 const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
-const url = "mongodb+srv://demo:avijeet1@cluster0.pyhq3.mongodb.net/food?authSource=admin&replicaSet=atlas-51btm7-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+const uri = `mongodb://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0-shard-00-00.mb2gs.mongodb.net:27017,cluster0-shard-00-01.mb2gs.mongodb.net:27017,cluster0-shard-00-02.mb2gs.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-l52uzd-shard-0&authSource=admin&retryWrites=true&w=majority`
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.once('open', _ => {
-    console.log('Database connected:')
+mongoose.connect(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
 })
-
-db.on('error', err => {
-    console.error('connection error:', err)
+const connection=mongoose.connection;
+connection.once('open', () => {
+    console.log('Connection established');
 })
+connection.on('error', function(err) {
+    console.error('MongoDB event error: ' + err);
+});
 
 
-// session configure
+//"" session configure
 app.use(session({
-    secret: process.env.COOKIE_SECRET,
+    secret: process.env.secret,
     resave: false,
     store: MongoDbStore.create({
-        mongoUrl: url
+        mongoUrl: uri
     }),
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 3600 * 24 }
@@ -60,7 +63,8 @@ app.set('view engine', 'ejs')
 
 require('./routes/web')(app)
 
-
+const PORT = process.env.PORT || 3100
 app.listen(PORT, () => {
     console.log(`Listening on  port ${PORT}`)
-})
+});
+
